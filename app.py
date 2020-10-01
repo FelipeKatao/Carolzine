5acefb2c8e563378c6d60e3d26ee77bc5f23dc1c
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-10-01 20:31:34 -0300

    update

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask 
+import os
+from flask import Flask
+from flask_cors import CORS
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "<h1>Hello world</h1>"
-@app.route('/index')
-def index(){
-    return "<h1>Funciona!!!</h1>"
-}
-if __name__ == '__main__':
-    app.run(debug=True, port=port)
+cors = CORS(app, resource={r"/*":{"origins": "*"}})
+
+@app.route("/", methods=['GET'])
+def index():
+    return "<h1>Hello World!</h1>"
+
+@app.route("/deploy", methods=['GET'])    
+def deploy():
+    return "<h1>Testando deploy GitHub x Heroku </h1>"
+
+def main():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    main()
