3f56ebbc3ab5a03abb7e87421d7329a15b8ec566
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-10-01 20:10:14 -0300

    update back end

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "<h1>Hello world</h1>"
-
+@app.route('/index')
+def index(){
+    return "<h1>Funciona!!!</h1>"
+}
 if __name__ == '__main__':
     app.run(debug=True)
