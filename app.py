0a83b557e8cceea6f646596fae6a44d12a6f4ef1
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-10-01 21:13:28 -0300

    update

--- app.py
+++ app.py
@@ -1,22 +1,15 @@
-import os
 from flask import Flask
-from flask_cors import CORS
-
 app = Flask(__name__)
 
-cors = CORS(app, resource={r"/*":{"origins": "*"}})
 
-@app.route("/", methods=['GET'])
-def index():
-    return "<h1>Hello World!</h1>"
+@app.route('/')
+def hello():
+    return "Hello World!"
 
-@app.route("/deploy", methods=['GET'])    
-def deploy():
-    return "<h1>Testando deploy GitHub x Heroku </h1>"
 
-def main():
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run()
