758bb2219ba83539c89f93b6b8e2312aaaad455b
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-10-07 20:53:24 -0300

    redirect for login page

--- src/pages/welcomepage.js
+++ src/pages/welcomepage.js
@@ -1,11 +1,16 @@
-import EngineApp from "../../Framework/engine.js"
-import login from "../components/login.js"
 
-let eng = new EngineApp()
+import login from "../components/login.js"
+import {routesCreated,eng} from "../routes/route.js"
+
 
 function Welcomepage(){
+    if(sessionStorage.getItem('user')!=undefined){
+        eng.routesEngine.goToLink("http://127.0.0.1:5500/index.html#workArea")
+    }
+    else{
     eng.componentsEngine.createComponent("pageWelcomeLogin",pageRender())
     eng.componentsEngine.renderComponent("footer","pageWelcomeLogin")
+    }
 }
 
 function pageRender(){
